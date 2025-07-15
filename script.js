0da78160c19e1600464580a7314bd772ca1f18ba
Elyza <elyzazairin@gmail.com>
2025-07-15 22:54:17 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
- document.querySelector("form").addEventListener("submit", function (e) {
+ window.addEventListener("load", function () {
+    alert("Selamat datang di profil ELYZA!");
+});
+ 
+document.querySelector("form").addEventListener("submit", function (e) {
     const nama = document.querySelector("input[name='nama']").value.trim();
     const pesan = document.querySelector("textarea[name='pesan']").value.trim();
     if (nama === "" || pesan === "") {
